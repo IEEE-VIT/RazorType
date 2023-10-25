385381039b8660e73a6291aff06ab2dcf5de5f30
Kristen Kelly <kekelly3333@gmail.com>
2023-10-26 09:32:03 +1100

    call results algorithm
    Automatically fetch results when the last letter of the last word is pressed, and the user is taken to /results

--- src/Components/WordAPI.jsx
+++ src/Components/WordAPI.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import { useRef } from "react";
@@ -10,11 +10,11 @@ import {
 } from "../Utils/getRandomParagraph";
 
 export default function WordAPI() {
-  const paragraph = getRandomParagraph();
+  const [paragraph, setParagraph] = useState(getRandomParagraph()); // Initialize paragraph once
   const inputRef = useRef(null);
+  const [inputText, setInputText] = useState("");
 
   const handleAccuracy = () => {
-    const inputText = inputRef.current.value;
     const { totalAccurateWords } = getStatistics(inputText, paragraph);
     const userAccuracy = Math.floor(
       (totalAccurateWords / PARAGRAPH_LENGTH) * 100
@@ -24,10 +24,33 @@ export default function WordAPI() {
   };
 
   const navigate = useNavigate();
+
   const buttonHandler = () => {
     const userAccuracy = handleAccuracy();
     navigate("/results", { state: { userAccuracy } });
   };
+
+  const handleInputChange = (e) => {
+    const text = e.target.value;
+    setInputText(text);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === " " || e.key === "Enter") {
+      // Check for last word here
+      const words = inputText.trim().split(" ");
+      if (words.length >= PARAGRAPH_LENGTH) {
+        buttonHandler();
+      }
+    }
+  };
+
+  useEffect(() => {
+    console.log("WordAPI component mounted");
+    return () => {
+      console.log("WordAPI component unmounted");
+    };
+  }, []);
 
   return (
     <>
@@ -38,7 +61,13 @@ export default function WordAPI() {
       >
         <div className="textHolder">
           <textarea className="fetchText" placeholder={paragraph} readOnly />
-          <textarea className="fetchTextOverlay" ref={inputRef}></textarea>
+          <textarea
+            className="fetchTextOverlay"
+            ref={inputRef}
+            value={inputText}
+            onChange={handleInputChange}
+            onKeyDown={handleKeyDown}
+          ></textarea>
           <button onClick={buttonHandler} className="accuracy">
             ACCURACY
           </button>
